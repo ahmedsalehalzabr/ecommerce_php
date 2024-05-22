90ee3029bc586629ee79a3faa40f815085a8f57a
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-05-21 16:37:39 -1200

    46

--- forgetpassword/verifycode.php
+++ forgetpassword/verifycode.php
@@ -13,14 +13,4 @@ $stmt->execute() ;
 
 $count = $stmt->rowCount() ; 
 
-if ($count > 0) {
- 
-    $data = array("users_approve" => "1") ; 
-
-    updateData("users" , $data , "users_email = '$email'");
-
-}else {
- printFailure("verifycode not Correct") ; 
-
-}
-?>
+ result($count) ;
--- test.php
+++ test.php
@@ -1,10 +1,12 @@
 <?php
 
-
 include "connect.php" ; 
 
 
-sendEmail("ahmedalzabr2@gmail.com" ,"hi" , "from function send email"); 
+getAllData("users" , "1 = 1" ) ; 
 
+
+
+ 
 
 ?>
